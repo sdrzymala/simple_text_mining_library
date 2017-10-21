e7b19c2b106e5d6501c4802511a756577614b7e7
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-21 19:47:12 +0200

    Refactoring. Add reading stop words from file

--- simple_text_mining_library/simple_text_mining_library/MineText.cs
+++ simple_text_mining_library/simple_text_mining_library/MineText.cs
@@ -1,7 +1,9 @@
 ﻿using simple_text_mining_library.Classes;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +11,34 @@ namespace simple_text_mining_library
 {
     public class MineText
     {
-        public TextMiningLanguage textMiningLanguage { get; set; }
+        TextMiningLanguage currentTextMiningLanguage;
+        string currentBasicStopWordsFilePath;
+
+        public TextMiningLanguage textMiningLanguage
+        {
+            get
+            {
+                return currentTextMiningLanguage;
+            }
+            set
+            {
+                currentTextMiningLanguage = value;
+
+                if (currentTextMiningLanguage == TextMiningLanguage.English)
+                {
+                    currentBasicStopWordsFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Dictionaries\stopwords_basic_en.txt");
+                }
+                else if (currentTextMiningLanguage == TextMiningLanguage.Polish)
+                {
+                    currentBasicStopWordsFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Dictionaries\stopwords_basic_en.txt");
+                }
+            }
+        }
 
         public string RemoveStopWordsFromText()
         {
+            List<string> allStopWords = File.ReadAllLines(currentBasicStopWordsFilePath).ToList().Where(x=> !x.Contains("#")).ToList();
+
             return "";
         }
     }
